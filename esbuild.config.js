fad18e06f7a84e23d528b9a6cf5326dc7750d7fa
Aditya Singh <aditya.singh@anyline.com>
2024-04-28 14:56:26 +0200

    now using esbuild context

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,18 +1,24 @@
-import * as esbuild from 'esbuild';
-import CssModulesPlugin from 'esbuild-css-modules-plugin';
+import * as esbuild from "esbuild";
+import CssModulesPlugin from "esbuild-css-modules-plugin";
 
-await esbuild.build({
-	outfile: 'public/build/index.js',
-	entryPoints: ['src/index.js'],
-	bundle: true,
-	platform: 'browser',
-	target: ['es2015'],
-	format: 'iife',
-	globalName: 'Demo',
-    metafile: true,
-    plugins: [
-        CssModulesPlugin({
-            inject: true
-        }),
-    ]
+const ctx = await esbuild.context({
+  outfile: "public/build/index.js",
+  entryPoints: ["src/index.js"],
+  bundle: true,
+  platform: "browser",
+  target: ["es2015"],
+  format: "iife",
+  globalName: "Demo",
+  metafile: true,
+  plugins: [
+    CssModulesPlugin({
+      inject: true,
+    }),
+  ],
 });
+
+await ctx.watch();
+await ctx.serve({
+  servedir: "public",
+});
+console.log(`Application is running at: http://localhost:8000`);
